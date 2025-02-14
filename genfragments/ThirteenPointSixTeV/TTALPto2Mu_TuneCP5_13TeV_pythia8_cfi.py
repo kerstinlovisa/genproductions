14f6ab389e1d9f73e0e71b23ab2696609ffdb3ca
kerstinlovisa <lovisa.rygaard@cern.ch>
2025-02-14 11:40:33 +0100

    genFragments for TTALPto2Mu for Run2 and Run3

---
+++ genfragments/ThirteenPointSixTeV/TTALPto2Mu_TuneCP5_13TeV_pythia8_cfi.py
@@ -0,0 +1,25 @@
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
+from Configuration.Generator.PSweightsPythia.PythiaPSweightsSettings_cfi import *
+
+generator = cms.EDFilter("Pythia8ConcurrentHadronizerFilter",
+    maxEventsToPrint = cms.untracked.int32(1),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(13600.),
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CP5SettingsBlock,
+        pythia8PSweightsSettingsBlock,
+        processParameters = cms.vstring(
+            "54:oneChannel = 1 1.0 100 13 -13"
+        ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CP5Settings',
+                                    'pythia8PSweightsSettings',
+                                    'processParameters'
+                                    )
+    )
+)
+
